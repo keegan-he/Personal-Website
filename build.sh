64b82bb6bc908af703795a0cc07d5d9154cbe21e
Keegan H <keeganslane@gmail.com>
2019-02-08 17:13:20 -0800

    updated build.sh to include additional commands for about, contact, photography, music

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-cat templates/top.html content/index.html templates/bottom.html > docs/index.html docs/about.html projects.html photography.html music.html contact.html
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/index.html templates/bottom.html > docs/about.html
+cat templates/top.html content/index.html templates/bottom.html > docs/contact.html
+cat templates/top.html content/index.html templates/bottom.html > docs/projects.html
+cat templates/top.html content/index.html templates/bottom.html > docs/music.html
+cat templates/top.html content/index.html templates/bottom.html > docs/photography.html
