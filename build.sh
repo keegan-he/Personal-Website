4dedf037c0f70f5076ef94e67d33e777bcb4dd4c
Keegan H <keeganslane@gmail.com>
2019-02-08 17:51:07 -0800

    added additional about.html, contact.html etc pages

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
 cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/index.html templates/bottom.html > docs/about.html
+cat templates/top.html content/index.html templates/bottom.html > docs/contact.html
+cat templates/top.html content/index.html templates/bottom.html > docs/projects.html
+cat templates/top.html content/index.html templates/bottom.html > docs/music.html
+cat templates/top.html content/index.html templates/bottom.html > docs/photography.html
