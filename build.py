8cf7e36fa850f0dc6a458cfc1938045a7337e75a
keegan-h <keeganslane@gmail.com>
2019-02-09 23:07:42 -0800

    Todo Notes added for other content pages

--- build.py
+++ build.py
@@ -3,3 +3,6 @@ middle = open("content/index.html").read()
 end = open("templates/bottom.html").read()
 combined = start + middle + end
 open("docs/index.html", "w+").write(combined)
+
+#To do: remove top and bottom from content/photography.html, content/about.html, 
+# content/projects.html and music.html. 
