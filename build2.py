ede0e0472509ca98cf47dc452becc88f1a453960
Keegan H <keeganslane@gmail.com>
2019-02-10 18:46:45 -0800

    added second python file for tweaking

---
+++ build2.py
@@ -0,0 +1,10 @@
+print("top test")
+
+top_temp = open("templates/top.html").read()
+content = open("content/index.html").read()
+bottom_temp = open("templates/bottom.html").read()
+
+index_html = top_temp + content + bottom_temp
+open("docs/index.html", "w+").write(index_html)
+
+print("bottom test")
